eb821738e33a9743ab5d4ea1ebada4370a7ee61f
Tomás Moreira <1231087@isep.ipp.pt>
2025-06-07 20:35:42 +0100

    home done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import LoadingScreen from "./sections/LoadingScreen"
 import Navbar from "./components/Navbar"
+import Home from "./sections/Home"
 
 export default ()=>{
     const [isLoaded, setIsLoaded] = useState<boolean>(false)
@@ -11,6 +12,7 @@ export default ()=>{
             }}/>}
             <div className={"min-h-screen transition-opacity duration-700 " + isLoaded ? "opacity-100" : "opacity-0" + " bg-black text-gray-100"}>
                 <Navbar/>
+                <Home/>
             </div>
         </>
     )
--- src/sections/LoadingScreen.tsx
+++ src/sections/LoadingScreen.tsx
@@ -20,9 +20,9 @@ export default ({onComplete} : {onComplete: any}) => {
     return(
         <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
             <div className="mb-4 text-4xl font-mono font-bold">
-                {text} <span className="animate-blink ml-1"> | </span>
+                {text} <span className="animate-blink">|</span>
             </div>
-            <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+            <div className="w-[400px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
                 <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
             </div>
         </div>
