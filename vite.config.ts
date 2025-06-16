c828924176adda791777db7f096c9c175a1d34c7
Tomás Moreira <1231087@isep.ipp.pt>
2025-06-17 00:19:07 +0100

    changing base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-website/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
